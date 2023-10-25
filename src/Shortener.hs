c6646f87b32b653ac2c5a7fc3e69f123968496e2
Ou42 <25629874+Ou42@users.noreply.github.com>
2023-10-24 23:52:07 -0700

    html form created

--- src/Shortener.hs
+++ src/Shortener.hs
@@ -4,6 +4,7 @@ module Shortener where
 
 import Text.Blaze.Html.Renderer.Text (renderHtml)
 import qualified Text.Blaze.Html5 as H
+import qualified Text.Blaze.Html5.Attributes as A
 import Web.Scotty
 
 shortener :: IO ()
@@ -14,3 +15,6 @@ shortener =
         H.html $
           H.body $ do
             H.h1 "Shortener (using blaze-html)"
+            H.form H.! A.method "post" H.! A.action "/" $ do
+              H.input H.! A.type_ "text" H.! A.name "url"
+              H.input H.! A.type_ "submit"
