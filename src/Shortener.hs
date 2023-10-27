5d06bc55e1fd51561bb377693d78e67108538a2f
Ou42 <25629874+Ou42@users.noreply.github.com>
2023-10-26 21:51:55 -0700

    IORef
    - ... a mutable reference to a value
    - used along with Text & Map
    - to store the URLs

--- src/Shortener.hs
+++ src/Shortener.hs
@@ -2,14 +2,20 @@
 
 module Shortener where
 
+import Control.Monad.IO.Class (MonadIO(liftIO))
+import Data.IORef (modifyIORef, newIORef)
+import Data.Map (Map)
+import qualified Data.Map as M
+import Data.Text (Text)
 import Text.Blaze.Html.Renderer.Text (renderHtml)
 import qualified Text.Blaze.Html5 as H
 import qualified Text.Blaze.Html5.Attributes as A
 import Web.Scotty
 
 shortener :: IO ()
-shortener =
-  scotty 3000 $
+shortener = do
+  urlsR <- newIORef (1 :: Int, mempty :: Map Int Text)
+  scotty 3000 $ do
     get "/" $
       html $ renderHtml $
         H.html $
@@ -18,3 +24,9 @@ shortener =
             H.form H.! A.method "post" H.! A.action "/" $ do
               H.input H.! A.type_ "text" H.! A.name "url"
               H.input H.! A.type_ "submit"
+    post "/" $ do
+      url <- param "url"
+      liftIO $ modifyIORef urlsR $
+        \(i, urls) ->
+          (i + 1, M.insert i url urls)
+      redirect "/"
