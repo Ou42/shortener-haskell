a7a61a7a8abb5434e29a0527034abe9342d8abbb
Ou42 <25629874+Ou42@users.noreply.github.com>
2023-10-28 22:17:21 -0700

    end of tut - access shortened URLs
    - code from tut
    - I was pretty close!
    - url is found and used, but doesn't redirect correctly

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Following tutorial: Stack Builders - Getting started with Haskell projects using Scotty
 
-[link](https://www.stackbuilders.com/blog/getting-started-with-haskell-projects-using-scotty/)
+[Tutorial link](https://www.stackbuilders.com/blog/getting-started-with-haskell-projects-using-scotty/)
+
+***NOTE!*** Tutorial Completed, but URL's are not `redirect`ing correctly. Possibly a `localhost` issue? or a CORS issue? or something else!
 
 - cabal-version: 2.4
 - blaze-html-0.9.1.2
--- src/Shortener.hs
+++ src/Shortener.hs
@@ -7,12 +7,9 @@ import Data.Foldable (for_)
 import Data.IORef (modifyIORef, newIORef, readIORef)
 import Data.Map (Map)
 import qualified Data.Map as M
-import Data.Maybe (fromJust)
-import Data.Text (Text, unpack)
--- import qualified Data.Text.Lazy.Encoding as T
--- import qualified Data.Text.Lazy.IO as T
-import Data.Text.Lazy (fromStrict)
-import qualified Network.HTTP.Types.Status as Status
+import Data.Text (Text)
+import qualified Data.Text.Lazy as LT
+import Network.HTTP.Types (status404)
 import Text.Blaze.Html.Renderer.Text (renderHtml)
 import qualified Text.Blaze.Html5 as H
 import qualified Text.Blaze.Html5.Attributes as A
@@ -42,36 +39,15 @@ shortener = do
         \(i, urls) ->
           (i + 1, M.insert i url urls)
       redirect "/"
-    get "/:id" $ do
-      urlID <- param "id"
-      -- if `param "url"` is used, get error, param not found
+    get "/:n" $ do
+      n <- param "n"
       (_, urls) <- liftIO $ readIORef urlsR
 
-      -- the following works, outputs to the terminal:
-      -- liftIO $ putStrLn $ "id = " <> urlID -- OUTPUTS TO THE TERMINAL
-
-      -- the following sorta works ...
-      -- redirect $ fromStrict $ fromJust $ M.lookup urlID urls
-
-      {-
-      let maybeDestURL = M.lookup urlID urls
-      case maybeDestURL of
-        Nothing -> status Status.status404
-        Just destURL -> redirect destURL
-
-        ERROR:
-        • Couldn't match expected type ‘Data.Text.Internal.Lazy.Text’
-              with actual type ‘Text’
-          NB: ‘Data.Text.Internal.Lazy.Text’
-                is defined in ‘Data.Text.Internal.Lazy’
-              ‘Text’ is defined in ‘Data.Text.Internal’
-      -}
-
-      -- the following outputs to the terminal
-      let maybeDestURL = M.lookup urlID urls
-      case maybeDestURL of
-        Nothing -> status Status.status404
-        -- Just destURL -> liftIO $ putStrLn $ unpack destURL
-        -- Just destURL -> liftIO $ putStrLn $ show destURL -- linter: why not use print ?!
-        -- Just destURL -> liftIO $ print destURL
-        Just destURL -> liftIO $ print (urlID, destURL)
+      case M.lookup n urls of
+        Just url -> redirect (LT.fromStrict url)
+        -- the above "works" in that it does lookup
+        -- the url, but it redirects to:
+        -- localhost:3000/<url>
+        -- and then says,
+        -- "404: File Not Found!"
+        Nothing  -> raiseStatus status404 "not found. Ya filthy animal!"
